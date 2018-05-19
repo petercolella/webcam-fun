b94f852dc39f178d1229ae6ab4a647a406bb6713
petercolella <peter.joseph.colella@gmail.com>
2018-05-18 23:34:24 -0400

    paint to canvas function working

--- scripts.js
+++ scripts.js
@@ -12,7 +12,18 @@ function getVideo() {
     })
     .catch(err => {
         console.error(`Permision Required.`, err);
-    })
-};
+    });
+}
+
+function paintToCanvas() {
+    const width = video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        ctx.drawImage(video, 0, 0, width, height);
+    }, 16);
+}
 
 getVideo();
